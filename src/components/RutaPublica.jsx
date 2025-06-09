2deb02addabdc5266179229840a7d46c19d56311
elaserrot <125906393+Jjuparela2007@users.noreply.github.com>
2025-06-09 10:31:47 -0500

    ACTUALIZACION RUTA

--- src/components/RutaPublica.jsx
+++ src/components/RutaPublica.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import Swal from 'sweetalert2';
-import API from '../api';
 const API_URL = "http://localhost:3001";
 
 export default function RutaPublica() {
