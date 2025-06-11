cb5f9e97b1aa54432bd77a1e5bc0dce1fd9a3737
leetosky27 <leetosky27@gmail.com>
2025-06-11 10:38:06 -0500

    Update Index.jsx

--- src/pages/Cliente/Index.jsx
+++ src/pages/Cliente/Index.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
-import API from "../../api/API";
+import API from '../../api/api';
 const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3001";
 console.log("API_URL:", API_URL);
 
